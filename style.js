e1689c3cce7fe5807f4b208a410395e1861208aa
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-08-29 11:00:26 +0100

    I made a calculator

---
+++ style.js
@@ -0,0 +1,12 @@
+function digit(Numbers){
+        input.value += Numbers
+}
+function solve(){
+    input.value = eval(input.value)
+}
+// function solve(Numbers){
+//     input.value += input.value
+// }
+function del(){
+    input.value = "";
+}
